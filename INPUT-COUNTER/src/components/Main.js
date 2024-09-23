9f580d24bec88947289ae9d45bff481f51ad08da
kapilsarkar <k.sarkar81@gmail.com>
2024-09-23 13:30:26 +0530

    updated

--- INPUT-COUNTER/src/components/Main.js
+++ INPUT-COUNTER/src/components/Main.js
@@ -1,30 +1,44 @@
 import { useState } from "react";
 
 const Main = () => {
-  const [input, setInput] = useState();
-  const [count, setCount] = useState(0);
-  const handleChange = (event) => {
-   const value = parseInt(event.target.value,10)
-   if (!isNaN(value)) {
-    setCount(value);
-  }
+  //State to hold the value entered in the input field
+  const [inputValue, setInputValue] = useState("");
+
+  //State to track the the current counter value
+  const [count, setCount] = useState(null);
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
   };
+
+  const counterResult = ()=>{
+     setCount(inputValue)
+  }
+
   const handlePlus = () => {
     setCount(count + 1);
   };
+
+  const handleMinus = ()=>{
+    setCount(count - 1)
+  }
 
   return (
     <>
       <h1 className="main-heading">Input-Counter</h1>
       <div className="container">
-        <input type="text" className="input-box" value={count} onChange={handleChange} />
-        {/* <p className="input-result">{input}</p> */}
+        <input
+          type="text"
+          className="input-box"
+          value={inputValue}
+          onChange={handleInputChange}
+        />
+        <p className="input-result">{counterResult}</p>
       </div>
       <div className="button-container">
         <button className="btnPlus" onClick={handlePlus}>
           Increment
         </button>
-        <button className="btnMinus">Decrement</button>
+        <button className="btnMinus" onClick={handleMinus}>Decrement</button>
       </div>
     </>
   );
