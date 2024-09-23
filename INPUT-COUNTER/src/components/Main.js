0b86560cc24273ccdb3eff14932dc2eabe0338ca
kapilsarkar <k.sarkar81@gmail.com>
2024-09-23 07:18:53 +0530

    somepartsadded

--- INPUT-COUNTER/src/components/Main.js
+++ INPUT-COUNTER/src/components/Main.js
@@ -2,11 +2,12 @@ import { useState } from "react";
 
 const Main = () => {
   const [input, setInput] = useState();
-  const [count, setCount] = useState();
+  const [count, setCount] = useState(0);
   const handleChange = (event) => {
-    setInput(event.target.value);
-
-    
+   const value = parseInt(event.target.value,10)
+   if (!isNaN(value)) {
+    setCount(value);
+  }
   };
   const handlePlus = () => {
     setCount(count + 1);
@@ -16,8 +17,8 @@ const Main = () => {
     <>
       <h1 className="main-heading">Input-Counter</h1>
       <div className="container">
-        <input type="text" className="input-box" onChange={handleChange} />
-        <p className="input-result">{input}</p>
+        <input type="text" className="input-box" value={count} onChange={handleChange} />
+        {/* <p className="input-result">{input}</p> */}
       </div>
       <div className="button-container">
         <button className="btnPlus" onClick={handlePlus}>
