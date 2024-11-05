759aeb11536c3f88040d7b07868c9cdd6deeff73
kapilsarkar <k.sarkar81@gmail.com>
2024-11-05 09:31:19 +0530

    somepartsadded

--- weather-app/src/components/Body.jsx
+++ weather-app/src/components/Body.jsx
@@ -1,17 +1,22 @@
-import { useEffect, useState } from "react";
-import {key} from "../utils/constant"
+import {useState } from "react";
+import { key } from "../utils/constant";
 const Body = () => {
-  const [searchText, setSearchText] = useState("");
-  
-  useEffect(()=>{
-     fetchData()
-  },[])
-
-  const fetchData= async()=>{
-    const data = await fetch(`https://api.weatherapi.com/v1/current.json?key=${key}&q=kolkata`);
-    const json = await data.json();
-    console.log(json)
-  }
+  const [searchCity, setSearchCity] = useState("");
+  const [weatherLocation, setWeatherLocation] = useState();
+  const [weatherCurrent,setWeatherCurrent] = useState();
+  const fetchWeather = async () => {
+    try {
+      const data = await fetch(
+        `https://api.weatherapi.com/v1/current.json?key=${key}&q=${searchCity.toLowerCase()}}`
+      );
+      const json = await data.json();
+      console.log(json);
+      setWeatherLocation(json.location);
+      setWeatherCurrent(json.current);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <div className="center-container">
@@ -19,9 +24,14 @@ const Body = () => {
         type="text"
         className="input-box"
         placeholder="Enter City Name..."
-        value={searchText} onChange={(e)=>setSearchText(e.target.value)}
+        value={searchCity}
+        onChange={(e) => setSearchCity(e.target.value)}
       />
-      <button className="searchBtn">Search</button>
+      <button className="searchBtn" onClick={fetchWeather}>
+        Search
+      </button>
+      <p>{weatherLocation?.name}</p>
+      <p>{weatherCurrent?.temp_c}</p>
     </div>
   );
 };
