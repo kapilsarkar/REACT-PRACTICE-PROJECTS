25f67cb1410e413c69f76d5bd2b34f3667c6be95
kapilsarkar <k.sarkar81@gmail.com>
2024-12-01 08:04:12 +0530

    somepartsadded

--- SOCIAL MEDIA FOLLOWERS/src/components/Body.jsx
+++ SOCIAL MEDIA FOLLOWERS/src/components/Body.jsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
 const Body = () => {
   const [btnFollow, setBtnFollow] = useState("Follow");
-  const [imgSrc, setImgSrc] = useState(
-    "https://images4.alphacoders.com/617/617495.jpg"
+  const [star1, setStar1] = useState(
+    "https://www.themecircle.net/wp-content/uploads/2019/04/social-media-buttons.gif"
   );
+  const [star2,setStar2] = useState()
   return (
     <>
       <div className="w-full flex flex-wrap gap-2 p-3 bg-yellow-500">
@@ -11,13 +12,36 @@ const Body = () => {
           <img
             className="rounded-full cursor-pointer"
             onClick={() =>
-              setImgSrc(
-                "https://stat4.bollywoodhungama.in/wp-content/uploads/2016/03/Hrithik-Roshan-54.jpg"
+              setStar1(
+                "https://images4.alphacoders.com/617/617495.jpg"
               )
             }
-            src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjvut5zxVEDY-hstpTd-bbtpkE0TZKz0wYgiA0bcgumHduokDOANfWMlxLVcvFzb4THBpDX8igXqq_cP5DhdNDwuiviGC82LhO9mvPidZV6iyejzvrqVVZIhEwEAWOABaPrTYWhw7T9BQxj/s280/hrithik_roshan2.jpg"
+            src="https://mumbaimirror.indiatimes.com/thumb/68432728.cms?resizemode=4&width=400"
           />
           <p className="text-center font-bold">Hrithik Roshan</p>
+          <button
+            className="text-center bg-green-900 text-white p-2 rounded-lg cursor-pointer"
+            onClick={() => {
+              btnFollow === "Follow"
+                ? setBtnFollow("UnFollow")
+                : setBtnFollow("Follow");
+            }}
+          >
+            {btnFollow}
+          </button>
+        </div>
+
+        <div className="w-32  text-center">
+          <img
+            className="rounded-full cursor-pointer"
+            onClick={() =>
+              setStar2(
+                "https://wallpapercat.com/w/full/c/3/1/19452-3840x2160-desktop-4k-virat-kohli-background-photo.jpg"
+              )
+            }
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Virat_Kohli_in_PMO_New_Delhi.jpg/220px-Virat_Kohli_in_PMO_New_Delhi.jpg"
+          />
+          <p className="text-center font-bold">Virat Kohli</p>
           <button
             className="text-center bg-green-900 text-white p-2 rounded-lg cursor-pointer"
             onClick={() => {
@@ -33,8 +57,9 @@ const Body = () => {
       <h3 className="text-center bg-green-900 font-bold p-2 text-white">
         YOUR FAVORITE SUPERSTAR
       </h3>
-      <div className="w-full bg-yellow-500 p-5">
-        <img className="w-full" src={imgSrc} />
+      <div className="w-full bg-yellow-500 p-5" style={{ backgroundImage: star1 }}>
+        <img className="w-full rounded-xl" src={star1} />
+        <img className="w-full rounded-xl" src={star2} />
       </div>
     </>
   );
