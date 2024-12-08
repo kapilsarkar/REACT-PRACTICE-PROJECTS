3e9de260a2718decaf439943f4e0810731a75dcb
kapilsarkar <k.sarkar81@gmail.com>
2024-12-08 08:51:28 +0530

    somepartsadded

--- SOCIAL MEDIA FOLLOWERS/src/components/Header.jsx
+++ SOCIAL MEDIA FOLLOWERS/src/components/Header.jsx
@@ -13,8 +13,8 @@ const Header = () => {
         Here are the top 10 highest tax-paying celebrities in FY 2024
       </h1>
 
-      <header className="w-full overflow-x-auto shadow-2xl sticky z-50 top-0">
-        <ul className="flex flex-wrap  mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+      <header className="w-60  shadow-2xl sticky z-50 top-0">
+        <ul className="flex flex-col  mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
           <li className=" text-center pb-2">
             <NavLink
               to="/Srk"
@@ -40,6 +40,195 @@ const Header = () => {
               }}
             >
               {srkFollow}
+            </button>
+          </li>
+          <li className=" text-center pb-2">
+            <NavLink
+              to="/Vijay"
+              className={({ isActive }) =>
+                `block py-2 pr-4 pl-3 duration-200 ${
+                  isActive ? "bg-green-950" : "bg-white"
+                }border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+              }
+            >
+              <img
+                className="w-36 rounded-full p-2"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_h8KEw8hQVvYtJeVeap8kAsmqVGeCFAzbEg&s"
+                alt="logo"
+              />
+            </NavLink>
+            <p>Thalapathy Vijay</p>
+            <button
+              className="text-center bg-green-900 text-white px-3 py-1 rounded-lg"
+              onClick={() => {
+                vijayFollow === "Follow"
+                  ? setVijayFollow("UnFollow")
+                  : setVijayFollow("Follow");
+              }}
+            >
+              {vijayFollow}
+            </button>
+          </li>
+          <li className=" text-center pb-2">
+            <NavLink
+              to="/Vijay"
+              className={({ isActive }) =>
+                `block py-2 pr-4 pl-3 duration-200 ${
+                  isActive ? "bg-green-950" : "bg-white"
+                }border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+              }
+            >
+              <img
+                className="w-36 rounded-full p-2"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_h8KEw8hQVvYtJeVeap8kAsmqVGeCFAzbEg&s"
+                alt="logo"
+              />
+            </NavLink>
+            <p>Thalapathy Vijay</p>
+            <button
+              className="text-center bg-green-900 text-white px-3 py-1 rounded-lg"
+              onClick={() => {
+                vijayFollow === "Follow"
+                  ? setVijayFollow("UnFollow")
+                  : setVijayFollow("Follow");
+              }}
+            >
+              {vijayFollow}
+            </button>
+          </li>
+          <li className=" text-center pb-2">
+            <NavLink
+              to="/Vijay"
+              className={({ isActive }) =>
+                `block py-2 pr-4 pl-3 duration-200 ${
+                  isActive ? "bg-green-950" : "bg-white"
+                }border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+              }
+            >
+              <img
+                className="w-36 rounded-full p-2"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_h8KEw8hQVvYtJeVeap8kAsmqVGeCFAzbEg&s"
+                alt="logo"
+              />
+            </NavLink>
+            <p>Thalapathy Vijay</p>
+            <button
+              className="text-center bg-green-900 text-white px-3 py-1 rounded-lg"
+              onClick={() => {
+                vijayFollow === "Follow"
+                  ? setVijayFollow("UnFollow")
+                  : setVijayFollow("Follow");
+              }}
+            >
+              {vijayFollow}
+            </button>
+          </li>
+          <li className=" text-center pb-2">
+            <NavLink
+              to="/Vijay"
+              className={({ isActive }) =>
+                `block py-2 pr-4 pl-3 duration-200 ${
+                  isActive ? "bg-green-950" : "bg-white"
+                }border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+              }
+            >
+              <img
+                className="w-36 rounded-full p-2"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_h8KEw8hQVvYtJeVeap8kAsmqVGeCFAzbEg&s"
+                alt="logo"
+              />
+            </NavLink>
+            <p>Thalapathy Vijay</p>
+            <button
+              className="text-center bg-green-900 text-white px-3 py-1 rounded-lg"
+              onClick={() => {
+                vijayFollow === "Follow"
+                  ? setVijayFollow("UnFollow")
+                  : setVijayFollow("Follow");
+              }}
+            >
+              {vijayFollow}
+            </button>
+          </li>
+          <li className=" text-center pb-2">
+            <NavLink
+              to="/Vijay"
+              className={({ isActive }) =>
+                `block py-2 pr-4 pl-3 duration-200 ${
+                  isActive ? "bg-green-950" : "bg-white"
+                }border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+              }
+            >
+              <img
+                className="w-36 rounded-full p-2"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_h8KEw8hQVvYtJeVeap8kAsmqVGeCFAzbEg&s"
+                alt="logo"
+              />
+            </NavLink>
+            <p>Thalapathy Vijay</p>
+            <button
+              className="text-center bg-green-900 text-white px-3 py-1 rounded-lg"
+              onClick={() => {
+                vijayFollow === "Follow"
+                  ? setVijayFollow("UnFollow")
+                  : setVijayFollow("Follow");
+              }}
+            >
+              {vijayFollow}
+            </button>
+          </li>
+          <li className=" text-center pb-2">
+            <NavLink
+              to="/Vijay"
+              className={({ isActive }) =>
+                `block py-2 pr-4 pl-3 duration-200 ${
+                  isActive ? "bg-green-950" : "bg-white"
+                }border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+              }
+            >
+              <img
+                className="w-36 rounded-full p-2"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_h8KEw8hQVvYtJeVeap8kAsmqVGeCFAzbEg&s"
+                alt="logo"
+              />
+            </NavLink>
+            <p>Thalapathy Vijay</p>
+            <button
+              className="text-center bg-green-900 text-white px-3 py-1 rounded-lg"
+              onClick={() => {
+                vijayFollow === "Follow"
+                  ? setVijayFollow("UnFollow")
+                  : setVijayFollow("Follow");
+              }}
+            >
+              {vijayFollow}
+            </button>
+          </li>
+          <li className=" text-center pb-2">
+            <NavLink
+              to="/Vijay"
+              className={({ isActive }) =>
+                `block py-2 pr-4 pl-3 duration-200 ${
+                  isActive ? "bg-green-950" : "bg-white"
+                }border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+              }
+            >
+              <img
+                className="w-36 rounded-full p-2"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_h8KEw8hQVvYtJeVeap8kAsmqVGeCFAzbEg&s"
+                alt="logo"
+              />
+            </NavLink>
+            <p>Thalapathy Vijay</p>
+            <button
+              className="text-center bg-green-900 text-white px-3 py-1 rounded-lg"
+              onClick={() => {
+                vijayFollow === "Follow"
+                  ? setVijayFollow("UnFollow")
+                  : setVijayFollow("Follow");
+              }}
+            >
+              {vijayFollow}
             </button>
           </li>
           <li className=" text-center pb-2">
--- SOCIAL MEDIA FOLLOWERS/src/components/Srk/Srk.jsx
+++ SOCIAL MEDIA FOLLOWERS/src/components/Srk/Srk.jsx
@@ -1,16 +1,48 @@
-const Srk = ()=>{
-    return(
-        <>
-        <div className="p-3 flex flex-wrap gap-2">
-            <img className="w-[36rem] mx-auto rounded-3xl"
-             src="https://img.theweek.in/content/dam/week/magazine/theweek/leisure/images/2023/2/11/63-Shah-Rukh-Khan.jpg"/>
-            <div>
-                <h3 className="text-center font-bold">Shah Rukh Khan</h3>
-                <p className="font-bold p-3">Dunki star Shah Rukh Khan has topped the list by paying an astounding Rs 92 crore in taxes. SRK&apos;s comeback movie Pathaan, released in January 2023, grossed over Rs 1000 crore worldwide. His second release of the year Jawaan exceeded the previous expectations to reach Rs 1100 crore mark. However, his last film Dunki could not perform on the same level as the previous two films, but it still managed to earn Rs 400 crore. Following the blockbuster year SRK was crowned as the highest paid actor in 2023.</p>
-            </div>
-        </div>
-        </>
-    )
-}
+import { useState } from "react";
+import { FcLike } from "react-icons/fc";
+import { FcDislike } from "react-icons/fc";
+const Srk = () => {
+  const [like, setLike] = useState(0);
+  const [dislike, setDislike] = useState(0);
 
-export default Srk
+  const Increment = () => {
+    setLike(like + 1);
+  };
+
+  const Decrement = () => {
+    setDislike(dislike + 1);
+  };
+  return (
+    <>
+      <div className="w-full p-3 flex flex-col md:flex-row flex-wrap gap-2">
+        <div className=" w-full md:w-[40%]">
+          <img
+            className="w-full rounded-3xl mx-auto"
+            src="https://img.theweek.in/content/dam/week/magazine/theweek/leisure/images/2023/2/11/63-Shah-Rukh-Khan.jpg"
+          />
+          <div className="flex gap-2 justify-center cursor-pointer">
+            <FcLike onClick={Increment} /> {like}
+            <FcDislike onClick={Decrement} />
+            {dislike}
+          </div>
+        </div>
+
+        <div className="w-full md:w-[55%]">
+          <h3 className="text-center font-bold">Shah Rukh Khan</h3>
+          <p className="font-bold p-3">
+            Dunki star Shah Rukh Khan has topped the list by paying an
+            astounding Rs 92 crore in taxes. SRK&apos;s comeback movie Pathaan,
+            released in January 2023, grossed over Rs 1000 crore worldwide. His
+            second release of the year Jawaan exceeded the previous expectations
+            to reach Rs 1100 crore mark. However, his last film Dunki could not
+            perform on the same level as the previous two films, but it still
+            managed to earn Rs 400 crore. Following the blockbuster year SRK was
+            crowned as the highest paid actor in 2023.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Srk;
