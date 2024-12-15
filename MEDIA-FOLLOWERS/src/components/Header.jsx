aa3c326c765603ac4ac0e6fe3c954c23bb6838cd
kapilsarkar <k.sarkar81@gmail.com>
2024-12-15 08:28:29 +0530

    somepartsadded

---
+++ MEDIA-FOLLOWERS/src/components/Header.jsx
@@ -0,0 +1,19 @@
+const Header = () => {
+  return (
+    <>
+      <header className="w-96 p-3 bg-yellow-600">
+        <ul>
+          <li className="text-center">
+            <img
+              className="w-36"
+              src="https://pbs.twimg.com/profile_images/690516151885934593/168sd2MB_400x400.jpg"
+            />
+            <h2>Hrithik Roashan</h2>
+          </li>
+        </ul>
+      </header>
+    </>
+  );
+};
+
+export default Header;
--- MEDIA-FOLLOWERS/src/main.jsx
+++ MEDIA-FOLLOWERS/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import Layout from "./components/Layout";
+import { createBrowserRouter,createRoutesFromElements,Route,RouterProvider } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route path="/" element={<Layout/>}/>
+))
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router}/>
+  </StrictMode>
+);
