35ec53085307efd679d7ac76101fb92493b5e83d
kapilsarkar <k.sarkar81@gmail.com>
2024-12-28 09:02:51 +0530

    somepartsadded

--- AVENGERS/src/components/Captain/Captain.jsx
+++ AVENGERS/src/components/Captain/Captain.jsx
@@ -1,15 +1,33 @@
 import { FcLike } from "react-icons/fc";
 import { FcDislike } from "react-icons/fc";
 import UseLikeDislike from "../UseLikeDislike";
-
+import { ImPower } from "react-icons/im";
+import { useState } from "react";
 const Captain = () => {
   const [like, disLike, LikeIncrement, DisLikeIncrement] = UseLikeDislike();
+  const [fillingPercentage, setFillingPercentage] = useState(0);
+
+  const fillTheJug = () => {
+    setFillingPercentage((prev) => {
+      if (prev === 0) return 1;
+      const change = prev + prev * 0.5;
+      if (change > 100) return 100;
+      return change;
+    });
+  };
+
+  const emptyTheJig = ()=>{
+    setFillingPercentage((prev)=>{
+      if(prev<1) return 0
+      return prev - prev *0.5;
+    })
+  }
   return (
     <>
-      <div className="w-full p-3 flex flex-col  media970px:flex-row flex-wrap gap-2 mt-10">
-        <div className=" w-full md:w-[40%]">
+      <div className="w-full p-3 flex flex-col  media970px:flex-row flex-wrap gap-2 ">
+        <div className="w-full md:w-[60%] mx-auto">
           <img
-            className="w-full rounded-3xl mx-auto animate-pulse"
+            className="w-full rounded-3xl mx-auto animate-pulse shadow-2xl"
             src="https://rare-gallery.com/thumbs/805402-Steve-Rogers-Captain-America-hero-Men-Heroes-comics.jpg"
           />
           <div className="flex gap-2 justify-center cursor-pointer">
@@ -19,19 +37,38 @@ const Captain = () => {
             {disLike}
           </div>
         </div>
-
-        <div className="w-full md:w-[55%] flex flex-wrap justify-center gap-2">
+        <h2 className="text-center p-2 bg-violet-950 text-2xl text-white font-bold shadow-2xl">
+          Captain America Power Details
+        </h2>
+        <div className="w-full flex flex-wrap justify-center gap-3 mt-2">
           <img
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHRhfCAECfhiHuCApvxkicpQ7bKE39NONotycAOz6FFVE3En1GqCluaCP-ggiXKnxktEw&usqp=CAU"
-            className="mt-5 w-[200px] h-[300px] relative overflow-hidden shadow-2xl rounded-3xl animate-bounce "
+            src="https://data.tooliphone.net/iskin/themes/5809/4261/preview-256.png"
+            className="mt-5 w-[200px] h-[200px] relative overflow-hidden shadow-2xl rounded-full animate-spin "
           />
 
-          <div className="w-[300px] h-[200px] border-gray-950 border-2 border-t-0 rounded-3xl relative overflow-hidden shadow-2xl">
-            <h3 className="text-center mt-2 p-1 font-bold text-xl">
-              Increase/Decrease Power
-            </h3>
-            <div className="w-full bg-blue-600 opacity-70 absolute bottom-0 rounded-b-2xl rounded-t-2xl"></div>
+          <div className="w-[300px] h-[200px]  border-t-0 rounded-3xl relative overflow-hidden shadow-2xl">
+            <div
+              className="w-full bg-blue-600 opacity-70 absolute bottom-0 rounded-b-2xl rounded-t-2xl"
+              style={{ height: `${fillingPercentage}%` }}
+            >
+              <span className="text-white font-bold  animate-pulse">
+                {Math.round(fillingPercentage)}%
+              </span>
+            </div>
           </div>
+        </div>
+        <div className=" flex flex-wrap gap-2 justify-center w-96 mx-auto cursor-pointer">
+          <button
+            className="bg-violet-900 rounded-full text-white p-3"
+            onClick={fillTheJug}
+          >
+            <ImPower />
+            Power Up
+          </button>{" "}
+          <button className="bg-violet-900 rounded-full text-white p-3" onClick={emptyTheJig}>
+            <ImPower />
+            Power Down
+          </button>
         </div>
       </div>
       <div className="w-full mt-2">
