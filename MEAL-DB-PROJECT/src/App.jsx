0f7f412754c297476261591f53c5786854f12385
kapilsarkar <k.sarkar81@gmail.com>
2025-02-12 13:13:42 +0530

    somepartsadded

--- MEAL-DB-PROJECT/src/App.jsx
+++ MEAL-DB-PROJECT/src/App.jsx
@@ -1,9 +1,12 @@
+import Body from "./components/Body";
+
 function App() {
   return (
     <>
       <h2 className=" bg-green-700 text-white p-3 text-center font-bold">
         MEAL DB PROJECT
       </h2>
+      <Body/>
     </>
   );
 }
--- MEAL-DB-PROJECT/src/components/Body.jsx
+++ MEAL-DB-PROJECT/src/components/Body.jsx
@@ -1,9 +1,31 @@
+import { useState } from "react";
+
 const Body = () => {
+  const [searchMeal, setSearchMeal] = useState("");
+
+  const fetchMeal = async () => {
+    const data = await fetch(
+      `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchMeal.toLowerCase()}`
+    );
+    const json = await data.json();
+    console.log(json);
+  };
+
   return (
     <>
-      <div className="flex items-center p-2">
-        <input type="text" className="p-2" />
-        <button>Search</button>
+      <div className="flex items-center justify-center gap-2 p-2">
+        <input
+          type="text"
+          className="p-2 border-2 rounded-md shadow-2xl"
+          value={searchMeal}
+          onChange={(e) => setSearchMeal(e.target.value)}
+        />
+        <button
+          className="bg-green-600 text-white p-2 rounded-md cursor-pointers"
+          onClick={() => fetchMeal()}
+        >
+          Search
+        </button>
       </div>
     </>
   );
