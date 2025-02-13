d800b7d767006a5851562541f9e3498d355e051e
kapilsarkar <k.sarkar81@gmail.com>
2025-02-13 17:27:16 +0530

    somepartsadded

--- MEAL-DB-PROJECT/src/components/FoodCard.jsx
+++ MEAL-DB-PROJECT/src/components/FoodCard.jsx
@@ -1,8 +1,13 @@
-const FoodCard = ()=>{
+/* eslint-disable react/prop-types */
+const FoodCard = ({detail})=>{
+    console.log(detail)
     return(
         <>
         <div>
-            
+            {detail.map((currItem,index)=>{
+                console.log(currItem)
+                <p>{index}</p>
+            })}
         </div>
         </>
     )
