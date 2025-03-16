42385748c1cb96d4f6eb69a203136551232fd723
kapilsarkar <k.sarkar81@gmail.com>
2025-03-16 08:02:20 +0530

    somepartsadded

---
+++ FOOD-APP/src/components/Body.jsx
@@ -0,0 +1,14 @@
+const Body = ()=>{
+    return(
+        <div>
+        <h2
+        className=" text-center text-2xl text-white bg-orange-600 mt-3 p-2 font-bold"
+        id="TopHeading"
+      >
+        Top Restaurants
+      </h2>
+        </div>
+    )
+}
+
+export default Body;
