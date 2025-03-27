36f5646c64c5c750c1635acd2966268cf6c29843
kapilsarkar <k.sarkar81@gmail.com>
2025-03-27 07:36:57 +0530

    somepartsadded

--- FOOD-APP/src/components/Body.jsx
+++ FOOD-APP/src/components/Body.jsx
@@ -1,23 +1,66 @@
 import { useEffect, useState } from "react";
 import { RESTAURANT_URL } from "../utils/constant";
 import TopRestaurant from "./TopRestaurant";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
   const [listOfRestaurants, setListOfRestaurant] = useState([]);
+  const [searchText, setSearchText] = useState("");
   useEffect(() => {
     fetchData();
   }, []);
+
+  const handleSearch = () => {
+    const filterData = listOfRestaurants.filter((res) => {
+      return res.info.name.toLowerCase().includes(searchText.toLowerCase());
+    });
+    setListOfRestaurant(filterData);
+  };
+
+  const allSearch = () => {
+    fetchData();
+    setSearchText("");
+  };
 
   const fetchData = async () => {
     const data = await fetch(RESTAURANT_URL);
     const json = await data.json();
     console.log(json);
-    console.log(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
-    setListOfRestaurant(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+    console.log(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    setListOfRestaurant(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
   };
 
-  return (
+  return listOfRestaurants.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="w-full">
+      <div className="p-2">
+        <div className="flex flex-wrap justify-center mt-3">
+          <input
+            className="cursor-pointer border-orange-500 rounded-xs shadow-2xl p-2 font-bold drop-shadow-3xl"
+            type="text"
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+            placeholder="Search..."
+          />
+          <button
+            className="cursor-pointer text-white bg-orange-600 border-none rounded-b-xs rounded-t-xs p-1.5 ml-1 font-bold shadow-2xl"
+            onClick={handleSearch}
+          >
+            Search
+          </button>
+          <button
+            className="cursor-pointer text-white bg-orange-600 border-none rounded-b-xs rounded-t-xs p-1.5 ml-2 font-bold shadow-2xl"
+            onClick={allSearch}
+          >
+            All Restaurants
+          </button>
+        </div>
+      </div>
       <h2
         className=" text-center text-2xl text-white bg-orange-600 mt-3 p-2 font-bold"
         id="TopHeading"
@@ -25,10 +68,8 @@ const Body = () => {
         Top Restaurants
       </h2>
       <div className="flex flex-wrap justify-center gap-1.5 mt-2 w-auto">
-        {listOfRestaurants.map((restaurant)=>{
-          return (
-            <TopRestaurant resData={restaurant}/>
-          )
+        {listOfRestaurants.map((restaurant) => {
+          return <TopRestaurant resData={restaurant} />;
         })}
       </div>
     </div>
