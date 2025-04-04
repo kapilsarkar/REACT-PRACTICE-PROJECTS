f18b379cd72a44cbec0f15f278273c0aa5f5fc26
kapilsarkar <k.sarkar81@gmail.com>
2025-04-04 08:06:24 +0530

    someparsadded

--- FOOD-APP/src/components/RestaurantMenu.jsx
+++ FOOD-APP/src/components/RestaurantMenu.jsx
@@ -21,9 +21,6 @@ const RestaurantMenu = () => {
   const { itemCards } =
     resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card;
 
-  console.log(
-    resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card
-  );
   const categories = resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter((c)=> c.card?.card?.["@type"] === "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory");
   console.log(categories)
   return (
