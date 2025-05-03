a799f7d2ab3477420d8a8ba03ff0545e1addbeaa
kapilsarkar <k.sarkar81@gmail.com>
2025-05-03 08:58:48 +0530

    somepartsadded

---
+++ TODO-REDUX-TOOLKIT/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import AddTodo from "./components/AddTodo";
+import Todos from "./components/Todos";
+
+function App() {
+  return (
+    <>
+      <h2 className=" text-2xl text-center">TODO-APP-REDUX-TOOLKIT</h2>
+      <AddTodo />
+      <Todos />
+    </>
+  );
+}
+
+export default App;
