cdd4b14cae7a86fddf75988f10187ad1c1ebba1d
kapilsarkar <k.sarkar81@gmail.com>
2025-05-13 13:00:50 +0530

    added

--- REDUX TOOLKIT PROJECTS/E-COMMERCE-APP/src/components/Cart.jsx
+++ REDUX TOOLKIT PROJECTS/E-COMMERCE-APP/src/components/Cart.jsx
@@ -1,6 +1,8 @@
-import { useSelector } from "react-redux";
-
+import { useDispatch, useSelector } from "react-redux";
+import { removeItems, clearCart } from "../utils/cartSlice";
 const Cart = () => {
+  const dispatch = useDispatch();
+
   // 1. Access items from Redux state
   const cartItems = useSelector((state) => state.cart.items);
 
@@ -8,15 +10,60 @@ const Cart = () => {
   const totalCount = cartItems.length;
 
   const totalPrice = cartItems.reduce((acc, item) => acc + item.price, 0);
+
+  const handleRemove = (id) => {
+    dispatch(removeItems({ id }));
+  };
+  const handleClearCart = () => {
+    dispatch(clearCart());
+  };
   return (
     <>
-      <div>Cart</div>
-      <div className="p-3 font-bold">
-        <h2 className="text-xl">Cart summary</h2>
-        <p>Total Items: {totalCount}</p>
-        <p>Total Price : {totalPrice}</p>
+      <div className=" flex justify-center flex-wrap">
+        <div className="grid gap-4 p-3">
+          {cartItems.map((item, index) => (
+            <div
+              key={index}
+              className="flex items-center gap-4 p-4 bg-amber-100 shadow-md rounded-md"
+            >
+              <img
+                src={item.thumbnail}
+                className="w-24 h-24 object-cover rounded-md"
+              />
+              <div className="p-1.5">
+                <h3 className="text-lg text-purple-700 font-semibold">
+                  {item.title}
+                </h3>
+                <p className="text-sm text-gray-600">{item.brand}</p>
+                <p className="text-sm text-gray-500">{item.category}</p>
+                <p className="text-red-600 font-bold mt-1">₹ {item.price}</p>
+              </div>
+              <button
+                className="text-white bg-red-600 px-3 py-1 rounded hover:bg-red-700 cursor-pointer"
+                onClick={() => handleRemove(item.id)}
+              >
+                Remove
+              </button>
+            </div>
+          ))}
+          {cartItems.length === 0 && (
+            <p className="text-center text-gray-600 font-medium mt-10">
+              Your cart is empty.
+            </p>
+          )}
+        </div>
+        <div className="p-3 font-bold bg-black shadow-md rounded-md  mt-2 text-white">
+          <h2 className="text-xl p-2 text-amber-500">Cart summary</h2>
+          <p className=" font-bold text-xs">Total Items: {totalCount}</p>
+          <p className=" font-bold text-xl">Total Price :₹ {totalPrice}</p>
+          <button
+            className="text-white bg-red-600 px-3 py-1 rounded hover:bg-red-700 cursor-pointer mt-3"
+            onClick={handleClearCart}
+          >
+            Clear Cart
+          </button>
+        </div>
       </div>
-       
     </>
   );
 };
