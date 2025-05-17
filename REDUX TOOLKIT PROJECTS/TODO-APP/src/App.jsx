0d517c1c521ac2f16e4e48ed8151239087937ff4
kapilsarkar <k.sarkar81@gmail.com>
2025-05-17 07:20:34 +0530

    added

--- REDUX TOOLKIT PROJECTS/TODO-APP/src/App.jsx
+++ REDUX TOOLKIT PROJECTS/TODO-APP/src/App.jsx
@@ -3,11 +3,11 @@ import Todos from "./components/Todos";
 
 function App() {
   return (
-    <>
+    <div className="bg-amber-300">
       <h2 className="bg-red-500 text-white text-center p-2 ">TODO-APP</h2>
       <AddTodo />
       <Todos/>
-    </>
+    </div>
   );
 }
 
