59bf2f0046e01a0e93aa3266096bf208c0a3041f
kapilsarkar <k.sarkar81@gmail.com>
2025-07-03 08:27:47 +0530

    somepartsadded

--- EXPENSE TRACKER APP/src/components/Home.jsx
+++ EXPENSE TRACKER APP/src/components/Home.jsx
@@ -1,11 +1,33 @@
-import ExpenseList from "./ExpenseList";
-import ExpenseSummary from "./ExpenseSummary";
+import Balance from "./Balance";
+import Expense from "./Expense";
+import Income from "./Income";
 
 const Home = () => {
   return (
-    <div>
-      <ExpenseSummary />
-      <ExpenseList />
+    <div className="w-full bg-white text-black dark:bg-gray-950 dark:text-white pt-5">
+      <div className=" flex justify-center flex-wrap gap-3 p-5">
+        <Income />
+        <Expense />
+        <Balance />
+      </div>
+      <div className="w-full sm:p-5 bg-white text-black dark:bg-gray-950 dark:text-white">
+        <form className=" w-72 sm:w-96 mt-2 border rounded-md border-white  p-3 shadow-lg">
+          <label className=" font-bold">Add an Expense</label>
+          <input
+            type="text"
+            placeholder="Add Income/Budget"
+            className=" p-1 sm:p-3 ml-1 shadow-md font-semibold rounded-md bg-white text-black dark:bg-gray-800 dark:text-white"
+          />
+          <div className=" flex flex-wrap gap-2 mt-2">
+            <button className=" px-2 py-1 sm:px-3 sm:py-2 bg-blue-500 text-white font-semibold rounded-md">
+              Add Income
+            </button>
+            <button className=" px-2 py-1 bg-red-500 text-white font-semibold rounded-md">
+              Reset
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
---
+++ EXPENSE TRACKER APP/src/components/Income.jsx
@@ -0,0 +1,12 @@
+const Income = () => {
+  return (
+    <>
+      <div className="w-96 bg-white text-black dark:bg-gray-950 dark:text-white h-48 border border-gray-300  rounded-md p-5 mt-2 shadow-2xl">
+        <h2 className=" font-bold text-3xl text-center text-green-500"> Income/Budget</h2>
+        <h3 className=" font-bold text-xl text-center text-green-500">₹ 0</h3>
+      </div>
+    </>
+  );
+};
+
+export default Income;
