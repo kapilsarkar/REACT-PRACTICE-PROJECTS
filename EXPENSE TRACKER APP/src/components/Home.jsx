0641737c8228a0e0e4405c0d5ca2df1f6c419394
kapilsarkar <k.sarkar81@gmail.com>
2025-07-04 08:37:19 +0530

    somepartsadded

--- EXPENSE TRACKER APP/src/components/Home.jsx
+++ EXPENSE TRACKER APP/src/components/Home.jsx
@@ -30,12 +30,12 @@ const Home = () => {
         <Expense />
         <Balance />
       </div>
-      <div className="w-full sm:p-5 bg-white text-black dark:bg-gray-950 dark:text-white">
+      <div className="w-full flex justify-center flex-wrap gap-2 sm:p-5 bg-white text-black dark:bg-gray-950 dark:text-white">
         <form
           onSubmit={handleAddIncome}
           className=" w-72 sm:w-96 mt-2 border rounded-md border-white  p-3 shadow-lg"
         >
-          <p className=" font-bold text-center">Add an Expense</p>
+          <p className=" font-bold text-center">Add Income/Budget</p>
 
           <input
             type="number"
@@ -59,6 +59,40 @@ const Home = () => {
             </button>
           </div>
         </form>
+        <form className=" w-96 sm:w-96 mt-2 border rounded-md border-white  p-3 shadow-lg">
+          <p className=" font-bold text-center mt-2">Add Expense</p>
+          <p className=" font-bold">Title</p>
+          <input
+            type="text"
+            placeholder="Ex: Car Payments"
+            className=" mt-2 p-1 sm:p-3  shadow-md font-semibold rounded-md bg-white text-black dark:bg-gray-800 dark:text-white"
+          />
+          <p className=" font-bold">Amount</p>
+          <input
+            type="number"
+            placeholder="Ex: 2000"
+            className=" mt-2 p-1 sm:p-3  shadow-md font-semibold rounded-md bg-white text-black dark:bg-gray-800 dark:text-white"
+          />
+          <p className=" font-bold mt-2 ">Add a Category To Your Expense</p>
+          <input
+            type="text"
+            placeholder="Ex: Groceries,Recharge"
+            className=" mt-2 p-1 sm:p-3  shadow-md font-semibold rounded-md bg-white text-black dark:bg-gray-800 dark:text-white"
+          />
+          <div className=" mt-2 flex flex-wrap gap-1">
+            <button className=" px-2 py-1 sm:px-3 sm:py-2 bg-blue-500 text-white font-semibold rounded-md">
+              Add Expense
+            </button>
+            <button className=" px-2 py-1 sm:py-2 bg-red-500 text-white font-semibold rounded-md">
+              Reset Expenses
+            </button>
+          </div>
+        </form>
+        <div className=" w-72 sm:w-96 mt-2 border rounded-md border-white  p-3 shadow-lg">
+             <button className=" px-2 py-1 sm:py-2 bg-blue-500 text-white font-semibold rounded-md">
+              Calculate Balance
+            </button>
+        </div>
       </div>
     </div>
   );
